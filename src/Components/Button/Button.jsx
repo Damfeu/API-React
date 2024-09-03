2d5fe78704cbdb27cab315229c6a33bfbfc1b913
KOKOUVI DAMAZ ADODODJI <damfeu20@gmail.com>
2024-09-03 09:22:29 +0100

    initial commit

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Button.css"
+
+export default function Button({ text, onClick, type, disabled }) {
+  return (
+    <div>
+      <button className='button' type={type} onClick={onClick} disabled={disabled}>{text || "opérations"}</button>
+
+    </div>
+  )
+}
